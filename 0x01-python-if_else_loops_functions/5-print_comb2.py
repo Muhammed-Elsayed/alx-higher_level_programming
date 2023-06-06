e8d5d7967ea2bde6e10326463347ad239033daab
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-06-06 23:45:12 +0300

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-for num in range(0, 100):
-    if (num != 99):
-        print(str(num).zfill(2), end=", ")
-
-    else:
-        print("99")
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
