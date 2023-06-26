de6fe5e7efdb201851e624d8c82925da1ff11fb5
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-06-26 16:36:44 +0300

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,6 @@ def safe_print_list(my_list=[], x=0):
     try:
         if x > len(my_list) - 1:
             raise Exception
-
         for index in range(0, x):
             print(my_list[index], end="")
         print("")
@@ -13,4 +12,3 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[index], end="")
         print("")
         return len(my_list)
-
