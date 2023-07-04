36401895ca02ba11b4128f78f7531042f8aa3ecb
[Mohamed Elsayed] <mohamedelsayed652002@gmail.com>
2023-07-04 19:11:17 +0300

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-iter = 0
 def magic_string():
-    global iter
-    iter += 1
-    return "BestSchool" + (", BestSchool" * (iter - 1))
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
