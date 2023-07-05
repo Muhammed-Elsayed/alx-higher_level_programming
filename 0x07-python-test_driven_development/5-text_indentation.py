b1b09b6df7ced59b1b37815a4a8eadd983909e3f
[Mohamed Elsayed] <mohamedelsayed652002@gmail.com>
2023-07-05 23:13:42 +0300

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""comment"""
+"""
+
+This module contains a function that indents texts
+
+"""
 
 
-def print_with_newlines(text):
-    '''printing two lines'''
-    for char in ['.', '?', ':']:
-        text = text.replace(char, char + '\n\n')
-    print(text)
+def text_indentation(text):
+    '''This function prints a text with 2 new lines after each ".", "?", or ":" '''
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    count = 0
+    while count < len(text) and text[count] == " ":
+        count = count + 1
+
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count = count + 1
+            while count < len(text) and text[count] == " ":
+                count = count + 1
+            continue
+        count = count + 1
