54b2ff3069065d215cdc1d6e94f19b58d34b8967
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-13 23:58:18 +0300

    3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,6 @@
 """documentation"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """class that is used to know if an object is an instance class"""
     return (isinstance(obj, a_class))
