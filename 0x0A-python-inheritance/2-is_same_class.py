a1a6c27a6e38924fc7a559b3db0e4f4fdaf15d4c
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-13 23:43:00 +0300

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""documentation"""
+
+
+def is_same_class(obj, a_class):
+    """class that is used to know if an object is an instance class"""
+    print(isinstance(obj, a_class))
