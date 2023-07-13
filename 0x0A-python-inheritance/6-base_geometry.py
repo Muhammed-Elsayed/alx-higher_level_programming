36c6dbc79d8a31e203ae8532013bd1aa45abbab9
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-14 00:16:26 +0300

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""documentation"""
+
+
+class BaseGeometry:
+    """retrun the area of the shape"""
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
