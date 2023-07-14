4082dd580a9c06a020d8255db0bf9e6d6a15cad5
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-14 19:32:49 +0300

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """reading an entire file and printing its content"""
     with open(filename, "r", encoding='utf-8') as myfile:
         content = myfile.read()
-        print(content)
+        print(content, end="")
