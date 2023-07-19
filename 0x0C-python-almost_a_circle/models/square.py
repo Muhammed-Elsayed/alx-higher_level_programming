9610517e87246b732e5e7c1b3c94ffd8e33c41e1
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-19 19:20:15 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,33 +1,70 @@
 #!/usr/bin/python3
 
-"""Rectangle module"""
+"""Square module.
+
+This module defines the Square class that inherits from Rectangle.
+A Square is a special type of rectangle with equal width and height.
+
+"""
 
 from rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class that inherets from rectangle and rectangle"""
+    """Square class that inherits from Rectangle.
+
+    Args:
+        size (int): The size of the square (equal width and height).
+        x (int): The x-coordinate of the square's position (default 0).
+        y (int): The y-coordinate of the square's position (default 0).
+        id (int): The unique identifier for the square (default None).
+
+    Attributes:
+        None
+
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """getter of the size property"""
+        """Get the size of the square.
+
+        Returns:
+            int: The size (equal width and height) of the square.
+
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """setter of the size property"""
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size to set for the square.
+
+        """
         self.__width = value
         self.__height = value
 
     def __str__(self):
-        """printing the object and class name and it's attributes"""
-        return "[Square] ({}) {}/{} - {}"\
-            .format(self.id, self.x, self.y, self.width)
+        """Return a string representation of the square.
+
+        Returns:
+            str: The string representation of the square.
+
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
-        """Updating the attributes of an object"""
+        """Update the attributes of the square.
+
+        Args:
+            *args: Non-keyword arguments (order: id, size, x, y).
+            **kwargs: Keyword arguments (attributes and their new values).
+
+        """
         if args:
             if len(args) >= 1:
                 self.id = args[0]
@@ -43,5 +80,10 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
-        """converting an object to a dictionary"""
+        """Convert the square object to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the square object.
+
+        """
         return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
