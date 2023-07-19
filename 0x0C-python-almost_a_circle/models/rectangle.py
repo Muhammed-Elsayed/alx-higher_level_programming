c239c60f54d076c08caa0abaf6f5309f8d864ca9
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-19 17:52:10 +0300

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,6 @@ class Rectangle(Base):
     """The rectangle class that inherets from the base class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """the construcor"""
         super().__init__(id)
         self.width = width
         self.height = height
