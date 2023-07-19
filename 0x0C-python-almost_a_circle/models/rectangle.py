3ae5d2a55e8d5c8b72edbba25ed53205564281d8
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-19 18:11:17 +0300

    update funct

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -143,6 +143,24 @@ class Rectangle(Base):
             for _ in range(self.height):
                 print(" " * self.x + "#" * self.width)
 
+    def update(self, *args, **kwargs):
+        """the updating function"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
     def __str__(self):
         """
         Return the representation of the Rectangle.
