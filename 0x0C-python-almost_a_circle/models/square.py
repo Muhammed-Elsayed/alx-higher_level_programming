eaa069b3e03773c179035ca28552071d08077fa4
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-19 19:18:04 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class that inherets from rectangle and rectangle inherets from base"""
+    """Square class that inherets from rectangle and rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
