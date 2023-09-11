ed43f15fbc5cdf795d6c65882c41196701914426
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-09-11 18:11:37 +0300

    9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,6 @@ if (isNaN(number)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < number; i++) {
-	  console.log('X'.repeat(number));
+    console.log('X'.repeat(number));
   }
 }
