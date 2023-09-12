df9f5546567257fa54df226e0161d3ce97a6375f
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-09-12 21:41:04 +0300

    5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 
 const Rectangle = require('./4-rectangle.js');
 
-const Square = class Square extends Rectangle {
+let Square = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
