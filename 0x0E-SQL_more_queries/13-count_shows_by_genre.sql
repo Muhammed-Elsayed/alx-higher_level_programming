8814fe944d47798c308f82716623793d22de2837
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-09-15 17:45:08 +0300

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- all genres from hbtn_0d_tvshows
- SELECT tv_genres.name AS genres, COUNT(tv_show_genres.genre_id) AS num FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genres ORDER BY num DESC;
+ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre ORDER BY number_of_shows DESC;
