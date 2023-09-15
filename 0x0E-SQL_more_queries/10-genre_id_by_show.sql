6dba816b2e43df19ce08d5798737f3e981ebaf83
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-09-15 17:22:03 +0300

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON show_id = tv_shows.id ORDER BYtitle, genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON show_id = tv_shows.id ORDER BY title, genre_id ASC;
