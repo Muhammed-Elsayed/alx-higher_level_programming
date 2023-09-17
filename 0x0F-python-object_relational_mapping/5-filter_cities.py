ee81ef7cd14dcee9c93048ac1e1752e4085b55ae
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-09-18 00:21:33 +0300

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,15 +15,15 @@ if __name__ == '__main__':
     cur.execute("""SELECT cities.id, cities.name, states.name
     FROM cities
     LEFT JOIN states ON cities.state_id = states.id
-    WHERE states.name = 'Texas'
     ORDER BY cities.id ASC""")
     results = cur.fetchall()
     count = 0
     for row in results:
-        print(row[1], end="")
-        count += 1
-        if (count != len(results)):
-            print("", end=", ")
-    print("")
+        if (row[2] == sys.argv[4]):
+            if count > 0:
+                print(", ", end="")
+            print(row[1], end="")
+            count += 1
+    print()
     cur.close()
     db.close()
