e227335c4d28617cf80b14bc7f9f977f7323ca23
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-09-18 00:38:57 +0300

    model_state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column("id", Integer, primary_key=True, unique=True,
+                autoincrement=True, nullable=False)
+    name = Column("name", String(128), nullable=False)
